1c0bc5daac2da8f6d5395dd3cdfb8ab19c4e86e8
arnaud-eb <arnaud.depierreux@easebest.com>
2024-07-09 07:52:00 +0200

    add express.js

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import http from "node:http";
+import app from "./server.ts";
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-  // console.log(res);
-  if (req.method === "GET" && req.url === "/") {
-    res.statusCode = 200;
-    res.end();
-    // res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write("<h1>Hello World!</h1>");
-    // res.end();
-  }
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
